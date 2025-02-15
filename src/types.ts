d0fb741e00aef21d520a9d2f5a7f3d213f61cb7a
kaneki003 <sakshamchauhan707@gmail.com>
2025-02-15 16:50:34 +0530

    end-to-end integegration with smart contract

--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,18 @@
 export interface Auction {
   id: string;
-  tokenAddress: string;
   name: string;
   description: string;
   imageUrl: string;
-  currentBid: number;
-  minBidAmount: number;
-  endTime: Date;
-  status: "active" | "ended";
-  owner: string;
-  bids: {
-    bidder: string;
-    amount: number;
-    timestamp: Date;
-  }[];
+  auctionType: number;
+  auctioneer: string;
+  tokenAddress: string;
+  tokenIdOrAmount: string;
+  startingBid: string;
+  highestBid: string;
+  highestBidder: string;
+  deadline: string;
+  minBidDelta: string;
+  deadlineExtension: string;
+  totalBids: string;
+  availableFunds: string;
 }
