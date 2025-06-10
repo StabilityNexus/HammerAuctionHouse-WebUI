0abddb999af140e9838e607493e95839b0f7641b
kaneki003 <sakshamchauhan707@gmail.com>
2025-06-10 23:41:15 +0530

    more removal of build files

--- .next/static/chunks/app_page_tsx_33287d0e._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_91060f96._.js",
-  "static/chunks/_f09cc3a6._.js"
-],
-    source: "dynamic"
-});
