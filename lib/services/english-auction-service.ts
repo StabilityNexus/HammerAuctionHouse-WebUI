272bb614c1d05664e135adc06239e0db4d782776
Aniket <iamaniketkumarmaner@gmail.com>
2026-01-25 12:14:43 +0530

    Fix typo in error logging message

--- lib/services/english-auction-service.ts
+++ lib/services/english-auction-service.ts
@@ -292,7 +292,7 @@ export class EnglishAuctionService implements IAuctionService {
       }
       return BigInt(0);
     } catch (error) {
-      console.error("Error occured while fetching user's current bid: ", error);
+      console.error("Error occurred while fetching user's current bid: ", error);
       throw error;
     }
   }
